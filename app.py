f565928c31bc58f084b2f43c4e3666f600006058
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-17 19:40:01 +0200

    feat: pre release

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import json
 import os
 import requests
-from datetime import datetime, timedelta
+import datetime
 from notion_client import Client
-from utils import load_database
+from utils import load_database, update_page
 
 
 notion = Client(auth=os.getenv('TOKEN'))
 
-# my_page = load_database(notion)
+my_page = load_database(notion)
 
 # with open('data.json', 'w') as f:
 #   json.dump(my_page, f, indent=2)
@@ -17,92 +17,108 @@ days_periodicity = ['Mo', 'Tue', 'Wed', 'Thu', 'Fri']
 special_days_property = ['Daily', 'On demand']
 periodicity_ranges = ['1t/w', '2t/w', '3t/w', '1t/2w', '1t/m', '2t/m', '2t/m', '1t/2m', '1t/3m']
 
+# with open('data.json', 'r') as f:
+  # my_page = json.load(f)
 
 
-with open('data.json', 'r') as f:
-  my_page = json.load(f)
+date_today = datetime.datetime(
+  datetime.datetime.now().year,
+  datetime.datetime.now().month,
+  datetime.datetime.now().day,
+)
 
-  date_today = datetime(
-    datetime.now().year,
-    datetime.now().month,
-    datetime.now().day,
-  )
+day = date_today.weekday()
 
-  day = date_today.weekday()
+for page in my_page['results']:
+  properties = page['properties']
 
-  for page in my_page['results']:
-    properties = page['properties']
+  if properties['Set date']['date'] and page['object'] == 'page':
+    if 'start' in properties['Set date']['date']:
+      set_date_start = list(map(int, properties['Set date']['date']['start'].split('-')))
+      set_date = datetime.datetime(*set_date_start)
+      due_date_property = list(map(int, page['properties']['Due Date']['date']['start'].split('-')))
+      due_date = datetime.datetime(*due_date_property)
+      result_set_date = None
+      result_due_date = None
 
-    if properties['Set date']['date']:
-      if 'start' in properties['Set date']['date']:
-        set_date_start = list(map(int, properties['Set date']['date']['start'].split('-')))
-        set_date = datetime(*set_date_start)
-        due_date_property = list(map(int, page['properties']['Due Date']['date']['start'].split('-')))
-        due_date = datetime(*due_date_property)
-        result_set_date = None
-        result_due_date = None
+      # handle set date property
+      if set_date > date_today:
+        print('Skip')
+        continue
 
-        # handle set date property
-        if set_date > date_today:
-          continue
+      elif set_date < date_today:
+        set_date_property = list(map(int, properties['Set date']['date']['start'].split('-')))
+        periodicity_property = properties['Periodicity']['multi_select']
+        periodicity = [i['name'] for i in periodicity_property]
 
-        elif set_date < date_today:
-          due_date_property = list(map(int, properties['Due Date']['date']['start'].split('-')))
-          due_date = datetime(*due_date_property)
-          set_date_property = list(map(int, properties['Set date']['date']['start'].split('-')))
-          set_date = datetime(*set_date_property)
-          periodicity_property = properties['Periodicity']['multi_select']
-          periodicity = [i['name'] for i in periodicity_property]
-          print('==========================')
+        # set due date
+        if 'Daily' in periodicity:
+          result_due_date = date_today
 
-          # set due date
-          if 'Daily' in periodicity:
-            result_due_date = date_today
+        else:
+          next_date = None
 
-          else:
-            next_date = None
+          for i in periodicity:
+            if i in days_periodicity:
+              if due_date.weekday() < days_periodicity.index(i):
+                next_date = due_date + datetime.timedelta(days = due_date.day + (days_periodicity.index(i) - due_date.weekday()))
 
+          if not next_date:
             for i in periodicity:
               if i in days_periodicity:
-                if due_date.weekday() < days_periodicity.index(i):
-                  next_date = due_date.day + (days_periodicity.index(i) - due_date.weekday())
-
-            if not next_date:
-              for i in periodicity:
-                if i in days_periodicity:
-                  next_date = due_date.day + (6 - due_date.weekday()) + days_periodicity.index(i)
-                  break
-
-            if next_date:
-              result_due_date = datetime(due_date.year, due_date.month, next_date)
-
-
-          # set set date
-          if result_due_date:
-            for i in periodicity:
-              if i == 'Daily':
-                result_set_date = result_due_date
+                next_date = due_date + datetime.timedelta(days = due_date.day + (6 - due_date.weekday()) + days_periodicity.index(i))
                 break
 
-              else:
-                if i in periodicity_ranges:
-                  range_parts = i.split('/')
-
-                  if range_parts[1] == 'w':
-                    result_set_date = datetime(result_due_date.year, result_due_date.month, result_due_date.day - 1)
-                    break
-
-                  elif range_parts[1] == 'm':
-                    result_set_date = datetime(result_due_date.year, result_due_date.month, result_due_date.day - 7)
-                    break
-
-                  elif range_parts[1] == '2w' or range_parts[1] == '3m':
-                    result_set_date = datetime(result_due_date.year, result_due_date.month, result_due_date.day - 14)
-                    break
-
-          print(result_due_date, result_set_date)
+          if next_date:
+            result_due_date = next_date
 
 
-        elif set_date == date_today:
-          # set status as To Do
-          continue
+        # set set date
+        if result_due_date:
+          for i in periodicity:
+            if i == 'Daily':
+              result_set_date = result_due_date
+              break
+
+            else:
+              if i in periodicity_ranges:
+                range_parts = i.split('/')
+
+                if range_parts[1] == 'w':
+                  result_set_date = result_due_date + datetime.timedelta(days = result_due_date.day - 1)
+                  break
+
+                elif range_parts[1] == 'm':
+                  result_set_date = result_due_date + datetime.timedelta(days = result_due_date.day - 7)
+                  break
+
+                elif range_parts[1] == '2w' or range_parts[1] == '3m':
+                  result_set_date = result_due_date + datetime.timedelta(days = result_due_date.day - 14)
+                  break
+
+        if result_due_date and result_set_date:
+          print('  set:', result_due_date, result_set_date, set_date)
+          r = update_page(notion, page['id'], {
+            'Due Date': {
+              'date': {
+                'start': result_due_date.strftime('%Y-%m-%d'),
+              },
+            },
+            'Set date': {
+              'date': {
+                'start': result_set_date.strftime('%Y-%m-%d'),
+              },
+            },
+          })
+          print(r.status_code)
+          print(r.text)
+
+      elif set_date == date_today:
+        print(page['id'])
+        r = update_page(notion, page['id'], {
+          'Status': {
+            'select': {
+              'name': 'TO DO'
+            }
+          }
+        })
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+import json
 import os
 from requests import request
+from requests.structures import CaseInsensitiveDict
+
+
 
 
 def load_database(notion):
@@ -17,8 +21,35 @@ def load_database(notion):
 
   return database
 
-def update_page(notion, page_id, data):
-  notion.pages.update(
-    page_id,
-    data,
+def update_page(notion, page_id, payload):
+  headers = {
+    'Authorization': f'Bearer {os.getenv("TOKEN")}',
+    'Notion-version': '2022-02-22',
+    'Content-type': 'application/json',
+  }
+
+  path = f'https://api.notion.com/v1/pages/{page_id}'
+
+  return request(
+    'PATCH',
+    path,
+    data=json.dumps({
+      'properties': payload
+    }),
+    headers = headers,
   )
+
+  # url = "https://api.notion.com/v1/pages/ef261ec1-fa7d-4b75-969a-9b541b99fa00"
+
+  # payload = '''{
+  #   "properties": {
+  #     "select": {
+  #       "name": "TO DO"
+  #     }
+  #   }
+  # }'''
+
+  # response = request("PATCH", url, json=payload, headers=headers)
+
+  # print(response.text)
+
